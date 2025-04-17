a28565879bc62ebafdcb2a271b7c18745e2a2c3f
mjkim <mjkim@bluedigm.com>
2025-04-17 13:53:46 +0900

    git 내용 정리 파일

---
+++ app/git.js
@@ -0,0 +1,6 @@
+//restore 파일 수정사항 되돌리기
+//git restore 파일명, git restore --staged 파일명, git restore .
+//revert 커밋 취소하기(사실상 새로운 커밋 생성하여 해당 커밋 삭제)
+//git revert 커밋ID, git revert HEAD(최신 커밋)
+//reset 시간 되돌리기(협업시 사용X)
+//git reset --hard 커밋ID, git reset --soft 커밋ID
